a67ce5105190536f8724f445d45b3f6a0cd54113
Francisco Sanchez <francisco.sanchez.g@usach.cl>
2024-11-13 09:45:12 -0300

    feat(backend):add configurations

---
+++ Backend/creditMicroservice/src/main/java/com/microservice/creditMicroservice/CreditMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.creditMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditMicroserviceApplication.class, args);
+	}
+
+}
