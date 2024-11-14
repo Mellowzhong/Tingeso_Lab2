296fdbe6c94456815a2b143671e3704c88a2aa95
Francisco Sanchez <francisco.sanchez.g@usach.cl>
2024-11-14 14:01:55 -0300

    build(backend):init the document microservice with config

---
+++ Backend/documentMicroservice/src/test/java/com/documentMicroservice/DocumentMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.documentMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
