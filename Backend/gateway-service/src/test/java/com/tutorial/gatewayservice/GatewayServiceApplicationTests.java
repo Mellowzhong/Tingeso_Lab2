025b4a52fdbbd6b8a93273fc951f3e98e0891c1d
Francisco Sanchez <francisco.sanchez.g@usach.cl>
2024-11-15 01:13:32 -0300

    feat(backend):modify the gateway

--- Backend/gateway-service/src/test/java/com/tutorial/gatewayservice/GatewayServiceApplicationTests.java
+++ Backend/gateway-service/src/test/java/com/tutorial/gatewayservice/GatewayServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.microservice.gateway;
+package com.tutorial.gatewayservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GatewayApplicationTests {
+class GatewayServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
