f5305b97565466764064e4519e08b5733cda787c
Francisco Sanchez <francisco.sanchez.g@usach.cl>
2024-11-19 13:07:48 -0300

    feat(backend):add gateway config

--- Backend/document-microservice/src/main/java/com/document_microservice/controllers/DocumentController.java
+++ Backend/document-microservice/src/main/java/com/document_microservice/controllers/DocumentController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/documents")
+@RequestMapping("/document")
 public class DocumentController {
 
     private final DocumentService documentService;
