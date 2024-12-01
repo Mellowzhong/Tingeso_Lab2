e1eef777af7f000b92adbe752dab4a94d8dba3b9
Francisco Sanchez <francisco.sanchez.g@usach.cl>
2024-11-30 21:16:12 -0300

    feat:change some files

--- Frontend/src/Utils/BaseUrl.js
+++ Frontend/src/Utils/BaseUrl.js
@@ -1,20 +1,16 @@
 import axios from "axios";
 
-// Verifica las variables de entorno cargadas por Vite
-console.log("Variables de entorno:", import.meta.env);
-
 // Obtiene la URL base de la API desde las variables de entorno de Vite
-const baseURL = import.meta.env.VITE_API_URL;
+const baseURL = localStorage.getItem("port");
 
-if (!baseURL) {
-  console.error("La variable de entorno VITE_API_URL no está definida");
-} else {
-  console.log("Base URL:", baseURL); // Muestra la URL base para verificar si se está cargando correctamente
-}
+console.log(baseURL);
 
 // Configura Axios con la baseURL
 const api = axios.create({
-  baseURL: baseURL || "http://localhost:3000", // Usa una URL por defecto si no se define
+  baseURL: `http://127.0.0.1:56079`,
+  headers: {
+    "Content-type": "application/json",
+  },
 });
 
 export default api;
